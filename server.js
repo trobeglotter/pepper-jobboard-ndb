737b7c337b3051dbb7993b818492d86c563b49b1
siusaidh <136972728+siusaidh@users.noreply.github.com>
2024-03-22 23:33:49 -0400

    copied env vars from railway

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const port = process.env.PGPORT || 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
